ee77feca19cfbd89cfc71d62fb110675b9a7db21
Marc <marc.magnin@gmail.com>
2015-04-24 10:41:04 +0200

    up

--- PetitesPoches/js/tools.js
+++ PetitesPoches/js/tools.js
@@ -6,13 +6,13 @@
     }
 })(jQuery);
 
-function delayLoop(collection,index, timer, action) {
+function delayLoop(collection, index, timer, action) {
     var i = 0;
     //  create a loop function
     setTimeout(function () {    //  call a 3s setTimeout when the loop is called
         action(collection[index]);
         index++;
-                         
+
         if (index < collection.length) {            //  if the counter < 10, call the loop function
             delayLoop(collection, index, timer, action);             //  ..  again which will trigger another 
         }                        //  ..  setTimeout()
@@ -25,16 +25,112 @@ $(document).ready(function () {
 
         if (event.deltaY != 0)
 
-            if ($('.modal-backdrop').length == 0) {
+            if ($('.modal-backdrop').length == 0 && orientation == "h") {
                 $('#booksContainer').stop().animate({ scrollLeft: '-=' + (400 * event.deltaY) + 'px' }, 200);
+            } else if ($('.modal-backdrop').length == 0 && orientation == "v") {
+
             }
-            
+
         //console.log(event.deltaX, event.deltaY, event.deltaFactor);
 
     });
+    var colW = 180;
+    var maxWidth = 500;
+    var orientation;
 
-   
-       
 
-    
+
+    var checkMasonryOrientation = function () {
+        if ($(window).width() > maxWidth && orientation != "h") {
+            orientation = "h"
+            var $container = $('.tilesContainer');
+
+            // change layout mode
+            $container.isotope({
+                resizable: false,
+                layoutMode: 'masonryHorizontal',
+                masonry: { rowHeight: 230 },
+            });
+            return;
+        } else if ($(window).width() < maxWidth && orientation != "v") {
+            orientation = "v";
+            var $container = $('.tilesContainer');
+
+            // change layout mode
+            $container.isotope({
+                resizable: false,
+                layoutMode: 'fitRows',
+
+            });
+            $container.isotope("shuffle");
+        }
+    }
+    $(window).resize(function () {
+        checkMasonryOrientation();
+    });
+    setTimeout(function () {
+        checkMasonryOrientation()
+    }, 200);
+
+
+
+
+    $.Isotope.prototype._getCenteredMasonryColumns = function () {
+
+        this.width = this.element.width();
+
+        var parentWidth = this.element.parent().width();
+
+        var colW = this.options.masonry && this.options.masonry.columnWidth || // i.e. options.masonry && options.masonry.columnWidth
+
+        this.$filteredAtoms.outerWidth(true) || // or use the size of the first item
+
+        parentWidth; // if there's no items, use size of container
+
+        var cols = Math.floor(parentWidth / colW);
+
+        cols = Math.max(cols, 1);
+
+        this.masonry.cols = cols; // i.e. this.masonry.cols = ....
+        this.masonry.columnWidth = colW; // i.e. this.masonry.columnWidth = ...
+    };
+
+    $.Isotope.prototype._masonryReset = function () {
+
+        this.masonry = {}; // layout-specific props
+        this._getCenteredMasonryColumns(); // FIXME shouldn't have to call this again
+
+        var i = this.masonry.cols;
+
+        this.masonry.colYs = [];
+        while (i--) {
+            this.masonry.colYs.push(0);
+        }
+    };
+
+    $.Isotope.prototype._masonryResizeChanged = function () {
+
+        var prevColCount = this.masonry.cols;
+
+        this._getCenteredMasonryColumns(); // get updated colCount
+        return (this.masonry.cols !== prevColCount);
+    };
+
+    $.Isotope.prototype._masonryGetContainerSize = function () {
+
+        var unusedCols = 0,
+
+        i = this.masonry.cols;
+        while (--i) { // count unused columns
+            if (this.masonry.colYs[i] !== 0) {
+                break;
+            }
+            unusedCols++;
+        }
+
+        return {
+            height: Math.max.apply(Math, this.masonry.colYs),
+            width: (this.masonry.cols - unusedCols) * this.masonry.columnWidth // fit container to columns that have been used;
+        };
+    };
 });
